7411010967bb19d6f729b6aefc293d4adfe57250
caldeira04 <joao.p.caldeira@icloud.com>
2026-06-07 01:01:01 -0300

    change font

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
-static const char *fonts[] = {"monospace:size=10"};
-static const char dmenufont[] = "monospace:size=10";
+static const char *fonts[] = {"MonaspiceNe Nerd Font:size=12:style=Bold"};
+static const char dmenufont[] = "MonaspaceNe Nerd Font:size=12:style=Bold";
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
@@ -110,7 +110,7 @@ static const Key keys[] = {
     {MODKEY | ShiftMask, XK_p, spawn, {.v = mpcplaylist}},
     {MODKEY, XK_p, spawn, {.v = mpcprev}},
     {MODKEY, XK_n, spawn, {.v = mpcnext}},
-    {MODKEY | ShiftMask, XK_space, spawn, {.v = mpctoggle}},
+    {MODKEY, XK_space, spawn, {.v = mpctoggle}},
 };
 
 /* button definitions */
